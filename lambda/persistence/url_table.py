699496ab91d27edbc26eba0e7d8b931ab6ef15be
Sam Kim <samkim90274@gmail.com>
2020-12-07 03:01:00 -0800

    Add persistence/url_table

---
+++ lambda/persistence/url_table.py
@@ -0,0 +1,60 @@
+import os
+import time
+
+import boto3
+
+URL_TABLE_NAME = os.environ["URL_TABLE_NAME"]
+
+URL_TABLE = boto3.resource("dynamodb").Table(URL_TABLE_NAME)
+
+
+class UrlTableSchema:
+    URL = "Url"
+    LAST_FETCHED = "LastFetched"
+    ERROR = "ErrorMessage"
+    TTL = "TimeToLive"
+
+
+def _calculate_ttl(ttl):
+    return int(time.time()) + ttl
+
+
+def _update_item(url, update_expression, expression_attribute_values):
+    kwargs = {
+        "Key": {UrlTableSchema.URL: url},
+        "UpdateExpression": update_expression,
+        "ExpressionAttributeValues": expression_attribute_values,
+    }
+    URL_TABLE.update_item(**kwargs)
+    print(f"Updated {URL_TABLE_NAME}. [{kwargs=}]")
+
+
+def get_all_url_items():
+    items = []
+    response = URL_TABLE.scan()
+    items = response["Items"]
+    while response.get("LastEvaluatedKey"):
+        response = URL_TABLE.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+        items += response["Items"]
+    return items
+
+
+def upsert_new_url(url, ttl):
+    _update_item(
+        url=url,
+        update_expression=f"set {UrlTableSchema.TTL}=:ttl",
+        expression_attribute_values={":ttl": _calculate_ttl(ttl)},
+    )
+
+
+def update_fetched_url(url, error=None):
+    update_expression = f"set {UrlTableSchema.LAST_FETCHED}=:last_fetched"
+    expression_attribute_values = {":last_fetched": int(time.time())}
+    if error:
+        update_expression += f", {UrlTableSchema.ERROR}=:error"
+        expression_attribute_values.update({":error": error})
+    _update_item(
+        url=url,
+        update_expression=update_expression,
+        expression_attribute_values=expression_attribute_values,
+    )
---
+++ tests/unit/persistence/url_table_test.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from unittest.mock import patch
+
+from freezegun import freeze_time
+
+from persistence.url_table import get_all_url_items, update_fetched_url, upsert_new_url
+
+
+@patch("persistence.url_table.URL_TABLE")
+def test_get_all_items(mock_table):
+    # Given
+    mock_table.scan.side_effect = (
+        {"Items": ["a"], "LastEvaluatedKey": True},
+        {"Items": ["b", "c"], "LastEvaluatedKey": True},
+        {"Items": ["d", "e"]},
+    )
+
+    # When
+    result = get_all_url_items()
+
+    # Then
+    assert result == ["a", "b", "c", "d", "e"]
+
+
+@freeze_time("2020-08-23")
+@patch("persistence.url_table.URL_TABLE")
+def test_upsert_new_url(mock_table):
+    # Given
+    url = "https://foo.com"
+    ttl = 24
+
+    # When
+    upsert_new_url(url, ttl)
+
+    # Then
+    mock_table.update_item.assert_called_once_with(
+        Key={"Url": url},
+        UpdateExpression="set TimeToLive=:ttl",
+        ExpressionAttributeValues={":ttl": int(datetime(2020, 8, 23).timestamp()) + ttl},
+    )
+
+
+@freeze_time("2020-08-23")
+@patch("persistence.url_table.URL_TABLE")
+def test_update_fetched_url(mock_table):
+    # Given
+    url = "https://foo.com"
+
+    # When
+    update_fetched_url(url)
+
+    # Then
+    mock_table.update_item.assert_called_once_with(
+        Key={"Url": url},
+        UpdateExpression="set LastFetched=:last_fetched",
+        ExpressionAttributeValues={":last_fetched": int(datetime(2020, 8, 23).timestamp())},
+    )
+
+
+@freeze_time("2020-08-23")
+@patch("persistence.url_table.URL_TABLE")
+def test_update_fetched_url_erro(mock_table):
+    # Given
+    url = "https://foo.com"
+    error = "test-error-message"
+
+    # When
+    update_fetched_url(url, error)
+
+    # Then
+    mock_table.update_item.assert_called_once_with(
+        Key={"Url": url},
+        UpdateExpression="set LastFetched=:last_fetched, ErrorMessage=:error",
+        ExpressionAttributeValues={
+            ":last_fetched": int(datetime(2020, 8, 23).timestamp()),
+            ":error": error,
+        },
+    )
