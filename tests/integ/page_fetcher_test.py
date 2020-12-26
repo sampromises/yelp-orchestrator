a0890f265071455d2fc493a24850fcb97aaa20ec
Sam Kim <samkim90274@gmail.com>
2020-12-25 21:42:50 -0800

    Fix page_fetcher integ test parallel run

--- tests/integ/page_fetcher_test.py
+++ tests/integ/page_fetcher_test.py
@@ -19,45 +19,8 @@ MIN_TIME = -1000000  # A value that will safely be less than any timestamp
 Url = namedtuple("Url", ["url", "last_fetched"])
 
 
-def test_happy_path():
-    items = (
-        Url("https://google.com", MIN_TIME),
-        Url("https://duckduckgo.com", MIN_TIME),
-        Url("https://yahoo.com", MIN_TIME),
-        Url("https://bing.com", MIN_TIME),
-        Url("https://apple.com", MIN_TIME),
-        Url("https://espn.com", MIN_TIME),
-        Url("https://yelp.com", MIN_TIME),
-        Url("https://reddit.com", MIN_TIME),
-        Url("https://python.org", MIN_TIME),
-        Url("https://wikipedia.org", MIN_TIME),
-        Url("https://stackoverflow.com", MIN_TIME + 1),  # Should be ignored
-    )
-    valid_items = items[:10]
-    try:
-        # Prepare UrlTable
-        _put_items(items)
-
-        # Invoke PageFetcher
-        invoke_time = int(time.time())
-        assert _invoke_page_fetcher()
-
-        # Verify UrlTable timestamps updated
-        updated_items = _get_items(valid_items)
-        for item in updated_items:
-            assert int(item.get("LastFetched")) >= invoke_time
-
-        # Verify S3 pages uploaded
-        pages = _get_pages(valid_items)
-        for page in pages:
-            assert page["ContentLength"]
-            assert page["LastModified"].timestamp() >= invoke_time
-    finally:
-        _delete_items(items)
-        _delete_pages(items)
-
-
-def test_single_url_error():
+def test_batch():
+    """Note: Cannot run other tests in parallel because PageFetcher will get the N oldest."""
     items = (
         Url("https://google.com", MIN_TIME),
         Url("https://duckduckgo.com", MIN_TIME),
@@ -69,9 +32,10 @@ def test_single_url_error():
         Url("https://reddit.com", MIN_TIME),
         Url("https://python.org", MIN_TIME),
         Url("ERROR_BUT_SHOULDNT_AFFECT_OTHER_URLS", MIN_TIME),
+        Url("https://stackoverflow.com", MIN_TIME + 1),  # Should be ignored
     )
     valid_items = items[:9]
-    invalid_item = items[-1]
+    invalid_item = items[-2]
     try:
         # Prepare UrlTable
         _put_items(items)
