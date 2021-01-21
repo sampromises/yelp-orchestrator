b313088cfac4feb9a5c5604f41ae9c4f9e269daa
Sam Kim <samkim90274@gmail.com>
2021-01-21 13:16:31 -0800

    [Update] Refactor page_fetcher to GET in parallel

--- src/yelp/page_fetcher.py
+++ src/yelp/page_fetcher.py
@@ -1,4 +1,7 @@
 import traceback
+from queue import Queue
+from threading import Thread
+from typing import Dict
 
 import requests
 
@@ -7,28 +10,67 @@ from yelp.persistence.page_bucket import upload_page
 from yelp.persistence.url_table import UrlTableSchema, get_all_url_items, update_fetched_url
 
 
+class FetchError(Exception):
+    def __init__(self, status_code, *args):
+        self.status_code = status_code
+        super().__init__(args)
+
+
+class BatchProcessor:
+    def __init__(self):
+        self.queue = Queue()
+        self.errors = []
+
+    def consumer(self):
+        item = self.queue.get()
+        url = item[UrlTableSchema.URL]
+        try:
+            # Fetch HTML
+            resp = requests.get(url)
+            print(
+                f"GET request finished. [status_code={resp.status_code}, content_length={len(resp.content)}]"
+            )
+
+            update_fetched_url(url, resp.status_code)
+
+            if resp.status_code != 200:
+                raise FetchError(
+                    resp.status_code,
+                    f"Fetch error. [status_code={resp.status_code}, text={resp.text}]",
+                )
+
+            upload_page(url, resp.content)
+        except FetchError as err:
+            self.errors.append(err)
+            traceback.print_exc()
+        except Exception as err:
+            update_fetched_url(url)
+            self.errors.append(err)
+            traceback.print_exc()
+        finally:
+            self.queue.task_done()
+
+    def process(self, items):
+        # Initialize Queue with sufficient size
+        self.queue = Queue(len(items) * 2)
+
+        # Create threads
+        for _ in range(len(items)):
+            t = Thread(target=self.consumer)
+            t.daemon = True
+            t.start()
+
+        # Enqueue requests
+        for item in items:
+            self.queue.put(item)
+
+        # Wait for all threads to finish
+        self.queue.join()
+
+
 def gather_batch():
     sorted_items = sorted(get_all_url_items(), key=lambda x: x.get(UrlTableSchema.LAST_FETCHED, 0))
     return sorted_items[:FETCH_BATCH_SIZE]
-
-
-def process_item(item):
-    url = item[UrlTableSchema.URL]
-    print(f"Processing item. [{url=}]")
-    try:
-        # Fetch HTML
-        resp = requests.get(url)
-        print(
-            f"GET request finished. [status_code={resp.status_code}, content_length={len(resp.content)}]"
-        )
-        if resp.status_code != 200:
-            raise Exception(f"Fetch error. [status_code={resp.status_code}, text={resp.text}]")
-
-        upload_page(url, resp.content)
-        update_fetched_url(url, resp.status_code)
-    except:
-        update_fetched_url(url)
-        raise
 
 
 def handle(event, context=None):
@@ -37,17 +79,11 @@ def handle(event, context=None):
     items = gather_batch()
     print(f"Gathered batch:\n{items}")
 
-    errors = []
-    for item in items:
-        try:
-            process_item(item)
-        except Exception as err:
-            errors.append(err)
-            traceback.print_exc()
-
-    if errors:
+    batch = BatchProcessor()
+    batch.process(items)
+    if batch.errors:
         raise Exception(
-            f"Encountered {len(errors)} total error(s) during processing. See execution log for errors."
+            f"Encountered {len(batch.errors)} total error(s) during processing. See execution log for errors."
         )
 
     return {"statusCode": 200}
--- tests/unit/page_fetcher_test.py
+++ tests/unit/page_fetcher_test.py
@@ -1,9 +1,9 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, call, patch
 
-import pytest
 from freezegun import freeze_time
+from tests.util import random_string
 from yelp import page_fetcher
-from yelp.page_fetcher import gather_batch, process_item
+from yelp.page_fetcher import BatchProcessor, gather_batch
 
 
 @patch("yelp.page_fetcher.get_all_url_items")
@@ -34,7 +34,7 @@ def test_gather_batch(mock_get_all_url_items):
 @patch("yelp.page_fetcher.upload_page")
 @patch("yelp.page_fetcher.update_fetched_url")
 @patch("yelp.page_fetcher.requests")
-def test_process_item_success(mock_requests, mock_update_fetched_url, mock_upload_page):
+def test_batch_process_single_success(mock_requests, mock_update_fetched_url, mock_upload_page):
     # Given
     url = "https://foo.com"
     mock_response = Mock()
@@ -44,19 +44,21 @@ def test_process_item_success(mock_requests, mock_update_fetched_url, mock_uploa
     mock_requests.get.return_value = mock_response
 
     # When
-    process_item({"PageUrl": url})
+    batch = BatchProcessor()
+    batch.process([{"PageUrl": url}])
 
     # Then
     mock_requests.get.assert_called_once_with(url)
     mock_upload_page.assert_called_once_with(url, "content")
     mock_update_fetched_url.assert_called_once_with(url, 200)
+    assert batch.errors == []
 
 
 @freeze_time("2020-08-23")
 @patch("yelp.page_fetcher.upload_page")
 @patch("yelp.page_fetcher.update_fetched_url")
 @patch("yelp.page_fetcher.requests")
-def test_process_item_error(mock_requests, mock_update_fetched_url, mock_upload_page):
+def test_batch_process_single_error(mock_requests, mock_update_fetched_url, mock_upload_page):
     # Given
     url = "https://foo.com"
     mock_response = Mock()
@@ -65,11 +67,86 @@ def test_process_item_error(mock_requests, mock_update_fetched_url, mock_upload_
     mock_response.text = "text"
     mock_requests.get.return_value = mock_response
 
-    with pytest.raises(Exception):
-        # When
-        process_item({"PageUrl": url})
+    # When
+    batch = BatchProcessor()
+    batch.process([{"PageUrl": url}])
 
-        # Then
-        mock_requests.get.assert_called_once_with(url)
-        mock_upload_page.assert_not_called()
-        mock_update_fetched_url.assert_called_once_with(url, 404)
+    # Then
+    mock_requests.get.assert_called_once_with(url)
+    mock_upload_page.assert_not_called()
+    mock_update_fetched_url.assert_called_once_with(url, 404)
+    assert len(batch.errors) == 1
+
+
+@freeze_time("2020-08-23")
+@patch("yelp.page_fetcher.upload_page")
+@patch("yelp.page_fetcher.update_fetched_url")
+@patch("yelp.page_fetcher.requests")
+def test_batch_process_multiple_success(mock_requests, mock_update_fetched_url, mock_upload_page):
+    # Given
+    count = 10
+
+    urls = [random_string() for _ in range(count)]
+
+    mock_responses = []
+    for _ in range(count):
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.content = "content"
+        mock_response.text = random_string()
+        mock_responses.append(mock_response)
+    mock_requests.get.side_effect = mock_responses
+
+    # When
+    batch = BatchProcessor()
+    batch.process([{"PageUrl": url} for url in urls])
+
+    # Then
+    mock_requests.get.assert_has_calls([call(url) for url in urls], any_order=True)
+    mock_update_fetched_url.assert_has_calls([call(url, 200) for url in urls], any_order=True)
+    mock_upload_page.assert_has_calls([call(url, "content") for url in urls], any_order=True)
+    assert batch.errors == []
+
+
+@freeze_time("2020-08-23")
+@patch("yelp.page_fetcher.upload_page")
+@patch("yelp.page_fetcher.update_fetched_url")
+@patch("yelp.page_fetcher.requests")
+def test_batch_process_multiple_success_single_error(
+    mock_requests, mock_update_fetched_url, mock_upload_page
+):
+    # Given
+    count = 10
+    failed_index = 4
+
+    urls = [random_string() for _ in range(count)]
+    failed_url = urls[failed_index]
+
+    mock_responses = []
+    for i in range(count):
+        mock_response = Mock()
+        mock_response.status_code = 200
+        if i == failed_index:
+            mock_response.status_code = 400
+        mock_response.content = "content"
+        mock_response.text = random_string()
+        mock_responses.append(mock_response)
+    mock_requests.get.side_effect = mock_responses
+
+    # When
+    batch = BatchProcessor()
+    batch.process([{"PageUrl": url} for url in urls])
+
+    # Assert successful URLs processed
+    success_urls = urls[:failed_index] + urls[failed_index + 1 :]
+    mock_requests.get.assert_has_calls([call(url) for url in success_urls], any_order=True)
+    mock_update_fetched_url.assert_has_calls(
+        [call(url, 200) for url in success_urls], any_order=True
+    )
+    mock_upload_page.assert_has_calls(
+        [call(url, "content") for url in success_urls], any_order=True
+    )
+
+    # Assert failed URL processed
+    mock_update_fetched_url.assert_has_calls([call(failed_url, 400)])
+    assert len(batch.errors) == 1
